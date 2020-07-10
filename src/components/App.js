526856e3dae669cb1ff7c08048cce1b38cb172ab
JimHui12 <jianghua.hui@uq.net.au>
2020-07-10 11:45:01 +1000

    react music master project

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+import Project from './Project';
+import SocialProfile from './SocialProfile';
+import Title from './Title';
+
+
+class App extends Component {
+    
+    render() {
+        return (
+            <div>
+                React App
+            </div>
+        )
+    }
+}
+
+
+
+ export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import '../src/index.scss'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    );
+
+
