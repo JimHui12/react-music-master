1f5591d6b24c2993517af57e9818bf63ab8e9def
JimHui12 <jianghua.hui@uq.net.au>
2020-07-13 15:12:08 +1000

    search bar and input added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
 import React, { Component} from 'react';
-import Project from './Project';
-import SocialProfile from './SocialProfile';
-import Title from './Title';
 
+class App extends Component {  
+    state = { artistQuery: ''}
 
-class App extends Component {
+    updateArtistQuery = event => {
+        console.log('event.target.value', event.target.value);
+        this.setState({ artistQuery: event.target.value});
+    }
+
+    handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+    }
+
+    searchArtist = () => {
+        console.log('this.state', this.state);
+    }
     
     render() {
         return (
             <div>
-                React App
+                <h2>Music Master</h2>
+                <input 
+                onChange={this.updateArtistQuery} 
+                onKeyPress={this.handleKeyPress}
+                placeholder='Search for an Artist'
+                />
+                <button onClick={this.searchArtist}>Search</button>
             </div>
-        )
+        );
     }
 }
 
