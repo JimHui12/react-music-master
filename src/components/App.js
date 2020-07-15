319c5d6fcca8c4badabfcb084b60ee880753172a
JimHui12 <jianghua.hui@uq.net.au>
2020-07-15 15:51:12 +1000

    pass down callbacks through component properties

--- src/components/App.js
+++ src/components/App.js
@@ -40,12 +40,6 @@ class App extends Component {
         return (
             <div>
                 <h2>Music Master</h2>
-                <input 
-                onChange={this.updateArtistQuery} 
-                onKeyPress={this.handleKeyPress}
-                placeholder='Search for an Artist'
-                />
-                <button onClick={this.searchArtist}>Search</button>
                 <Artist artist={this.state.artist} />
                 <Tracks tracks={this.state.tracks} />
             </div>
